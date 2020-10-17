50e723ce27d2c0aba806f8b41cc906af3991a7bd
L4ZZA <pietro.lazz@gmail.com>
2020-10-17 13:46:38 +0100

    Animated moving player colliding with enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    int damage;
+    [SerializeField]
+    float minSpeed;
+    [SerializeField]
+    float maxSpeed;
+    [SerializeField]
+    Player playerScript;
+
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.FindGameObjectWithTag(Player.LayerTag).GetComponent<Player>();
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == Player.LayerTag)
+        {
+            playerScript.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static string LayerTag => "Player";
+
+    [SerializeField]
+    float speed = 1;
+    [SerializeField]
+    int health = 1;
+
+    float input;
+
+    Rigidbody2D playerBody;
+    Animator animator;
+
+
+    const string RUNNING_PARAM_NAME = "isRunning";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input != 0)
+        {
+            animator.SetBool(RUNNING_PARAM_NAME, true);
+        }
+        else
+        {
+            animator.SetBool(RUNNING_PARAM_NAME, false);
+        }
+
+        if (input > 0)
+        {
+            // rotate right
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (input < 0)
+        {
+            // zero rotation (so face left)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+        playerBody.velocity = new Vector2(input * speed * Time.deltaTime, playerBody.velocity.y);
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
