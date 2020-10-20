bc1a1cb491f9fbf8a56212dee3131881605bea77
L4ZZA <pietro.lazz@gmail.com>
2020-10-20 16:48:39 +0100

    Added tons of effects to menu scene and added audio mixers

---
+++ Assets/Scripts/ButtonSFX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSFX : MonoBehaviour
+{
+    AudioSource source;
+    public AudioClip hoverSFX;
+    public AudioClip clickSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    public void HoverSound()
+    {
+        if (hoverSFX)
+            source.PlayOneShot(hoverSFX);
+    }
+
+    public void ClickSound()
+    {
+        if (clickSFX)
+            source.PlayOneShot(clickSFX);
+    }
+}
--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -6,6 +6,16 @@ public class MusicManager : MonoBehaviour
 {
     private static MusicManager instance;
 
+    static float maxMolume = 0.5f;
+    static float maxSfxVolume = 1f;
+
+    public static float master = maxMolume;
+    public static float volume = maxMolume;
+    public static float sfxVolume = maxSfxVolume;
+    
+    //public static bool master = true;
+    //public static bool sfx    = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,4 +29,12 @@ public class MusicManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
+    public static void ToggleMasterVolume()
+    {
+
+    }
+
+
 }
